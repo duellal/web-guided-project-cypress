80004c165e01d9a9d89efb020b168f8c2bca3112
duellal <alexandriaduell@gmail.com>
2022-08-01 15:11:24 -0400

    Added tests to check for dom elements + to check the inputs on the form

--- README.md
+++ README.md
@@ -16,3 +16,20 @@ being used by another application.
 - Run `npm run server` to spin up the quotes API on `http://localhost:3333/api/quotes`.
 - Run `npm start` to compile the React project and spin up the app on `http://localhost:1234`.
 - Run `npm test` to run tests using Cypress.
+
+## Notes:
+For cypress to run tests (npm test) you need to have:
+ - npm run server
+ - npm start
+
+  * You need both the front end AND back end to work before the tests can work.
+
+
+## Q+A:
+What is beforeEach()?
+ - use for code you want to run before the tests run; necessary set up for tests to run. The code in the beforeEach() that you definitely need is the navigation to the application - otherwise, you cannot test it.
+
+How do you make the application remember the objects in the array - even on refresh (instead of it resetting to the original array)?
+ - b/c there is a back end - the inputtes quotes are being posted to the back end array. They then "live" in the same place so on a refresh, the state is grabbing all of the quotes.
+
+ 
--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -4,10 +4,52 @@ describe('Quotes app', () => {
    beforeEach(() => {
       //arbitrary code we want running before the tests run
       cy.visit('http://localhost:1234')
-   })
+   });
+
    //Tests:
    it('sanity test - making sure tests work', () => {
+      //'expect' is an assertion
+      //there can be many assertions for each test inside the 'it' statement
+      //usually the assertions are logically grouped together
       expect(1 + 2).to.equal(3)
+      expect(2 + 3).not.to.equal(6)
+      expect({}).not.to.equal({}) //not strict equality ==
+      //.equal() === .eql()
+      expect({}).to.eql({}) //strict equality ===
+   });
+
+   it('playing - selecting elements from the dom', () => {
+      cy.get('input[name="text"]').should('exist');
+      cy.get('input[name=foobar]').should('not.exist')
+   });
+
+   //Grab 3 things - my code:
+   //second input - author name input
+   //submit quote button
+   //cancel button
+   it('grabbing elements from dom alone', () => {
+      cy.get('input[name="author"').should('exist');
+      cy.get('button[id="submitBtn"]').should('exist');
+      cy.get('button[id="cancelBtn"]').should('exist');
+   });
+
+   //Class Code:
+   it('grabbing elements from dom class', () => {
+      cy.get('input[name="author"]').should('exist')
+      cy.get('button[id="submitBtn"]').should('exist');
+      cy.get('button[id="cancelBtn"]').should('exist')
+      //Two ways to do .contains():
+      //Case Sensitive
+      cy.contains('Submit Quote');
+      //Not Case Sensitive
+      cy.contains(/submit quote/i)
    })
-})
+
+   //Checking typed inputs
+   it('can type inputs', () => {
+      //grab inputs -> assert that they're empty -> type in them -> assert the typed input is there
+      cy.get('input[name="text"]').should("have.value", "")
+      cy.get('input[name="text"]').type("have fun learning")
+   })
+});
 
