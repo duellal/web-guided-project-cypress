3b7638bb0768336a4c3a0ed3ac78af6381cf43c2
duellal <alexandriaduell@gmail.com>
2022-08-01 14:16:57 -0400

    Added sanity test

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,13 @@
 // write tests here
+describe('Quotes app', () => {
+   //Our tests go here
+   beforeEach(() => {
+      //arbitrary code we want running before the tests run
+      cy.visit('http://localhost:1234')
+   })
+   //Tests:
+   it('sanity test - making sure tests work', () => {
+      expect(1 + 2).to.equal(3)
+   })
+})
+
